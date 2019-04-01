9cef1c81b6b046547d6b5726537546aaa93933bb
Ben Dibuz <BDibuz@abrasive-form.com>
2019-04-01 11:01:33 -0500

    added monsters, added level up stats

--- js/monster.js
+++ js/monster.js
@@ -14,6 +14,10 @@ const monsters = [];
 monsters.push(monsterCreator('Ryan', 100, 2, 2, 1, 10, 500));
 monsters.push(monsterCreator('Ben', 100, 3, 1, 1, 10, 500));
 monsters.push(monsterCreator('Math Homework', 100, 5, 1, 2, 30, 1000));
+monsters.push(monsterCreator('All Your Exes', 100, 5, 1, 2, 30, 1000));
+monsters.push(monsterCreator('Looming Deadline', 100, 5, 1, 2, 30, 1000));
+monsters.push(monsterCreator('High Cost of Living', 100, 5, 1, 2, 30, 1000));
+monsters.push(monsterCreator('Medical Debt', 100, 5, 1, 2, 30, 1000));
 
 export const monsterGenerator = () => {
   const monsterNumber = Math.floor(Math.random()*monsters.length);
--- js/player.js
+++ js/player.js
@@ -13,6 +13,14 @@ export const player = {
 export const updatePlayerGoldAndXp = (player, monster) => {
   player.gold = player.gold + monster.goldGiven;
   player.xp = player.xp + monster.xpGiven;
+    if (player.xp >= player.xpToNextLevel){
+        player.xp = 0;
+        player.level++;
+        player.xpToNextLevel = player.xpToNextLevel*1.2;
+        player.health = player.health*1.2;
+        player.attack = player.attack*1.2;
+        player.defense = player.defense*1.2;
+    }
 }
 
 // module.exports = {
