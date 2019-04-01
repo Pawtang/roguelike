4344b7401fe1b871381d64e824a5d41354f1fdf0
ryantsai <rtsai0104@gmail.com>
2019-03-31 19:56:15 -0500

    Other stuff

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { monsters, monsterGenerator } = require('./monsters');
-const { maze } = require('./maze');
+const { monsters, monsterGenerator } = require('./monster');
+const { generateNewMaze } = require('./maze');
 const { player, updatePlayerGoldAndXp } = require('./player');
 
 let currentMonster = monsterGenerator();
@@ -7,3 +7,29 @@ console.log(currentMonster);
 console.log(player);
 updatePlayerGoldAndXp(player, currentMonster);
 console.log(player);
+
+let maze = generateNewMaze(); // generate initial maze
+console.log(maze);
+
+let gameNotBeaten = true, currentCommand;
+
+document.onkeydown = (e) => {
+  switch (e.keyCode) {
+    case 37:
+        alert('left');
+        break;
+    case 38:
+        alert('up');
+        break;
+    case 39:
+        alert('right');
+        break;
+    case 40:
+        alert('down');
+        break;
+  }
+}
+
+while(gameNotBeaten) {
+  currentCommand = Document.get
+}
--- maze.js
+++ maze.js
@@ -1,24 +1,36 @@
-const gridSize = 5;
+const generateNewMaze = () => {
+  const gridSize = 5;
 
-let maze = [];
-// Track the room we're in
-// Track what type of rooms we've hit
-const roomPercentages = [0.3, 0.3, 0.1, 0.1, 0.2];
+  let maze = [];
+  // Track the room we're in
+  // Track what type of rooms we've hit
+  const roomPercentages = [0.3, 0.3, 0.1, 0.1, 0.2];
 
-for(let i = 0; i < gridSize; i++) {
-  maze[i] = [];
-  for(let j = 0; j < gridSize; j++) {
-    maze[i][j] = {
-      leftDoor: false,
-      rightDoor: false,
-      upDoor: false,
-      downDoor: false,
-      event: null,
-      hasBeenTraveled: false
-    };
+  for(let i = 0; i < gridSize; i++) {
+    maze[i] = [];
+    for(let j = 0; j < gridSize; j++) {
+      maze[i][j] = {
+        // leftDoor: false,
+        // rightDoor: false,
+        // upDoor: false,
+        // downDoor: false,
+        // event: null,
+        // hasBeenTraveled: false,
+      };
+    }
   }
+
+  // set entrance
+  maze[4][0].isEntrance = true;
+  maze[4][0].rightDoor = true;
+
+  // set exit
+  maze[0][4].isExit = true;
+  maze[0][4].leftDoor = true;
+
+  console.log(maze);
 }
 
 module.exports = {
-  maze
+  generateNewMaze
 };
