e63de232c1383249b28cf07ec9830df14b65619a
Ben Dibuz <ben.dibuz@gmail.com>
2019-04-05 15:44:01 -0500

    fix merge

--- js/events.js
+++ js/events.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const battle = {
   eventName: "Battle"
 }
@@ -16,9 +15,6 @@ const BossRoom = {
 }
 
 const events = ["battle", "battle", "battle", "battle", "treasureRoom", "shopRoom", "eventlessRoom"];
-=======
-const events = ["battle", "battle", "battle", "battle", "treasureRoom", "shopRoom", "eventlessRoom","eventlessRoom", "eventlessRoom"];
->>>>>>> ef1b4bf77ea45cad2bcf09d5bac306f657e87953
 
 export const eventGenerator = () => {
   const eventNumber = Math.floor(Math.random()*events.length);
